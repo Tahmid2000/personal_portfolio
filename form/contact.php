45c2fd710010b8312eb8b7fa78253da8b37d851d
Tahmid Imran <tahmidimran1@gmail.com>
2021-01-03 14:12:12 -0600

    Final product

--- form/contact.php
+++ form/contact.php
@@ -1,73 +1,54 @@
 <?php
-/*
- *  CONFIGURE EVERYTHING HERE
- */
 
-// an email address that will be in the From field of the email.
-$from = 'Demo contact form <sender@yourdomain.com>';
+if ($_POST) {
+    $visitor_name = "";
+    $visitor_email = "";
+    $email_subject = "";
+    $visitor_message = "";
+    $email_body = "<div>";
 
-// an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <tahmidimran1@gmail.com>';
-
-// subject of the email
-$subject = 'New message from contact form';
-
-// form field names and their translations.
-// array variable name => Text to appear in the email
-$fields = array('InputName' => 'Name', 'InputEmail' => 'Email', 'InputSubject' => 'Subject', 'InputMessage' => 'Message');
-
-// message that will be displayed when everything is OK :)
-$okMessage = 'Your message successfully submitted. Thank you, I will get back to you soon!';
-
-// If something goes wrong, we will display this message.
-$errorMessage = 'There was an error while submitting the form. Please try again later';
-
-/*
- *  LET'S DO THE SENDING
- */
-
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-error_reporting(E_ALL & ~E_NOTICE);
-
-try {
-
-    if (count($_POST) == 0) throw new \Exception('Form is empty');
-
-    $emailText = "You have a new message from your contact form\n=============================\n";
-
-    foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email 
-        if (isset($fields[$key])) {
-            $emailText .= "$fields[$key]: $value\n";
-        }
+    if (isset($_POST['InputName'])) {
+        $visitor_name = filter_var($_POST['InputName'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Visitor Name:</b></label>&nbsp;<span>" . $visitor_name . "</span>
+                        </div>";
     }
 
-    // All the neccessary headers for the email.
-    $headers = array(
-        'Content-Type: text/plain; charset="UTF-8";',
-        'From: ' . $from,
-        'Reply-To: ' . $from,
-        'Return-Path: ' . $from,
-    );
+    if (isset($_POST['InputEmail'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['InputEmail']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>" . $visitor_email . "</span>
+                        </div>";
+    }
 
-    // Send email
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+    if (isset($_POST['InputSubject'])) {
+        $email_subject = filter_var($_POST['InputSubject'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>" . $email_subject . "</span>
+                        </div>";
+    }
 
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-} catch (\Exception $e) {
-    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-}
+    if (isset($_POST['InputMessage'])) {
+        $visitor_message = htmlspecialchars($_POST['InputMessage']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>" . $visitor_message . "</div>
+                        </div>";
+    }
 
+    $recipient = "tahmidimran1@gmail.com";
+    $email_body .= "</div>";
 
-// if requested by AJAX request return JSON response
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $encoded = json_encode($responseArray);
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+        . 'Content-type: text/html; charset=utf-8' . "\r\n"
+        . 'From: ' . $visitor_email . "\r\n";
 
-    header('Content-Type: application/json');
-
-    echo $encoded;
-}
-// else just display the message
-else {
-    echo $responseArray['message'];
+    if (mail($recipient, $email_subject, $email_body, $headers)) {
+        echo "<p>Thank you for contacting me, $visitor_name.</p>";
+    } else {
+        echo '<p>Sorry, you\'re email did not go through.</p>';
+    }
+} else {
+    echo '<p>Something went wrong</p>';
 }
